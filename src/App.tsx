9c4c1d9dab9cb2156f6743a1cb4789247f49f872
Patrick-Tomic <pat12tomic@gmail.com>
2025-08-28 10:55:38 -0400

    home page development

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
  import Navbar from './components/navbar'
 import './App.css'
-
+import stock from './assets/stock.png'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main className='flex flex-col '>
+    <main className='flex flex-col border-2 border-black border-solid '>
      <Navbar />
-     <div className='h-[100vh]' id='heroScreen'>
-ffff
+     <div id='hero' className='h-[100vh] w-full bg-[./assets/stock.png] bg-cover bg-center'  >
+      <p className='w-[45vw] font-[1000] text-black text-[50px] text-black  ml-[40vw] mt-[10vh]'>UNLOCK YOUR CHILD'S POTENTIAL WITH PERSONALIZED ABA THERAPY</p>
      </div>
     </main>
   )
