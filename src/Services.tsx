b678d82132629f260b57da3ceb58744eb652a101
Patrick-Tomic <pat12tomic@gmail.com>
2025-09-10 15:40:47 -0400

    update footer in services page

--- src/Services.tsx
+++ src/Services.tsx
@@ -7,7 +7,7 @@ export default function Services() {
         <div id="heroC" className="h-[100vh] w-full flex items-center justify-center">
              <p id='heroText' className='w-[45vw] font-new text-[50px]  ml-[0vw] mt-[5vh]'>Our Services</p>
         </div>
-        
+        <Footer />
         </>
     )
 }
